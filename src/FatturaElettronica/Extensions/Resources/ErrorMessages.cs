10b3c62cbdcdd70d98173da4f04024694b269dc9
Giuseppe Lippolis <workgroupengineering@outlook.com>
2020-04-20 12:54:23 +0200

    Replace ErrorMessages.resx with ErrorMessages.json for same issue with dotnet build  3.0.103 SDK

---
+++ src/FatturaElettronica/Extensions/Resources/ErrorMessages.cs
@@ -0,0 +1,91 @@
+﻿// FatturaElettronica.Extensions.Resources.ErrorMessages
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FatturaElettronica.Extensions.Resources
+{
+    internal class ErrorMessages : Dictionary<string, string>
+    {
+        private static readonly ErrorMessages dictionary;
+
+        internal static string FirmaException
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(FirmaException), out var result);
+                return result;
+            }
+        }
+
+        internal static string IdCodiceIsMissing
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(IdCodiceIsMissing), out var result);
+                return result;
+            }
+        }
+
+        internal static string IdFiscaleIsMissing
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(IdFiscaleIsMissing), out var result);
+                return result;
+            }
+        }
+
+        internal static string IdPaeseIsWrongOrMissing
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(IdPaeseIsWrongOrMissing), out var result);
+                return result;
+            }
+        }
+
+        internal static string LastBillingNumberIsTooLong
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(LastBillingNumberIsTooLong), out var result);
+                return result;
+            }
+        }
+
+        internal static string PfxIsMissing
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(PfxIsMissing), out var result);
+                return result;
+            }
+        }
+
+        internal static string SignatureException
+        {
+            get
+            {
+                dictionary.TryGetValue(nameof(SignatureException), out var result);
+                return result;
+            }
+        }
+
+        static ErrorMessages()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using (Stream source = typeof(ErrorMessages).Assembly.GetManifestResourceStream("FatturaElettronica.Extensions.Resources.ErrorMessages.json"))
+            {
+                using (StreamReader streamReader = new StreamReader(source))
+                {
+                    using (JsonTextReader reader = new JsonTextReader(streamReader))
+                    {
+                        dictionary = serializer.Deserialize<ErrorMessages>(reader);
+                    }
+                }
+            }
+        }
+    }
+}
